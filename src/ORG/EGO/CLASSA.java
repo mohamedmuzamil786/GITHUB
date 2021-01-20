c76a6c73c10372daa72bd9444e34eb91853efbfe
mohamedmuzamil786 <moahmedmuzamil03@gmail.com>
2021-01-20 19:34:38 +0530

    done

---
+++ src/ORG/EGO/CLASSA.java
@@ -0,0 +1,15 @@
+package ORG.EGO;
+
+public class CLASSA {
+	public static void main(String[] args) {
+	int a=10;
+	int b=20;
+	int c=30;
+	int d=40;
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(d);
+	System.out.println("sf");
+}
+}
